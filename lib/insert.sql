f81660454d77d172f0e141b817003f0dbdd12986
Jasmine Carter <jasmine.carter87@gmail.com>
2019-12-29 19:48:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name, age, color, gender) VALUES ('Mr. Chocolate', 1, 'Brown', 'Male')
---INSERT INTO bears (name, age, color, gender) VALUES ('Rowdy', 5, 'Red', 'Female')
+INSERT INTO bears (name, age, color, gender) VALUES ('Rowdy', 5, 'Red', 'Female')
 --INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 10, 'Female', 'Black', 'Calm', 'TRUE')
 --INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'Female', 'Olive Green', 'Militant', TRUE)
 --INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'Female', 'Brown', 'Shy', TRUE)
