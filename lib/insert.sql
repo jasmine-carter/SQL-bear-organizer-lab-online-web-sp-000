abaa7b5a43dce77ab77ffe263494751b3b0b7bea
Jasmine Carter <jasmine.carter87@gmail.com>
2019-12-29 19:51:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, color, gender) VALUES ('Mr. Chocolate', 1, 'Brown', 'Male');
 INSERT INTO bears (name, age, color, gender) VALUES ('Rowdy', 5, 'Red', 'Female');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 10, 'Female', 'Black', 'Calm', 'TRUE');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'Female', 'Olive Green', 'Militant', TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'Female', 'Brown', 'Shy', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 90, 'Male', 'Green', 'Sour', 1);
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Tabitha', 10, 'Female', 'Black', 'Calm');
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Sergeant Brown', 5, 'Female', 'Olive Green', 'Militant');
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Melissa', 6, 'Female', 'Brown', 'Shy');
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Grinch', 90, 'Male', 'Green', 'Sour');
 INSERT INTO bears (name, age, gender, color) VALUES ('Wendy', 3, 'Female', 'Red');
 INSERT INTO bears (name, age, gender) VALUES (NULL, NULL, NULL);
